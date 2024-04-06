49a7aa3b131466c469553a1edc49d056291c4eb6
mustafadansh <162048608+mustafadansh@users.noreply.github.com>
2024-04-06 11:02:55 -0700

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { massage: "Enter first number", type: "number", name: "firstNumber" },
+  { massage: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    massage: "Select one of the oparator to perform the oparation",
+    type: "list",
+    name: "oparator",
+    choices: ["Addition", "Subtraction", "Multipilication","Division"],
+  },
+]);
+
+// Conditional statement
+
+if( answer.oparator === "Addition") {
+   console.log(answer.firstNumber + answer.secondNumber);
+}
+ else if( answer.oparator === "Subtraction" ) {
+  console.log(answer.firstNumber - answer.secondNumber);
+}
+  else if( answer.oparator === "Multipilication" ) {
+  console.log(answer.firstNumber * answer.secondNumber);
+}
+ else if( answer.oparator === "Division" ) {
+  console.log(answer.firstNumber / answer.secondNumber);
+} 
+else {
+  console.log( "Please select valid oparator ");
+}
+
